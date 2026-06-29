fb1238775859fc99cc3530bd65c87a36e0db2d5e
Gershom Maes <gershommaes@protonmail.com>
2026-06-29 15:34:13 -0400

    <automated>

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,20 @@
 import '@gershy/clearing';
-import { Context, Flower, PetalTerraform } from '@gershy/lilac';
-
+import phrasing                                       from '@gershy/util-phrasing';
+import * as tf                                        from './util/terraform.ts';
+import * as aws                                       from './util/aws.ts';
+import { type Context, Flower, PetalTerraform, Soil } from '@gershy/lilac';
+import type { AnyLambda, LambdaBase }                            from '@gershy/lilac-lambda';
 // admin, write, query
 
-export class Storage extends Flower {
+class BinDbQuery { constructor(args: any) {} }
+class BinDbWrite { constructor(args: any) {} }
+class BinDbAdmin { constructor(args: any) {} }
+
+export class BinDb extends Flower {
   
   protected name: string;
   protected bucket: null | PetalTerraform.Base;
-  protected accessors: { mode: 'view' | 'mark' | 'keep', baseKey: null | string, lambda: Lambda<any, any, any, any, any> }[];
+  protected accessors: { mode: 'query' | 'write' | 'admin', baseKey: null | string, lambda: LambdaBase<any, any, any, any, any, any> }[];
   constructor(args: { name: string }) {
     super();
     this.name = args.name;
@@ -35,90 +42,71 @@ export class Storage extends Flower {
     return { bucket: this.getName(ctx), baseKey };
   }
   
-  addAccessor(ctx: Context, mode: 'view', baseKey: null | string, lambda: Lambda<any, any, any, any, any>): JsfnInst<typeof LambdaStorageViewer>;
-  addAccessor(ctx: Context, mode: 'mark', baseKey: null | string, lambda: Lambda<any, any, any, any, any>): JsfnInst<typeof LambdaStorageMarker>;
-  addAccessor(ctx: Context, mode: 'keep', baseKey: null | string, lambda: Lambda<any, any, any, any, any>): JsfnInst<typeof LambdaStorageKeeper>;
-  addAccessor(ctx: Context, mode: 'view' | 'mark' | 'keep', baseKey: null | string, lambda: Lambda<any, any, any, any, any>): any {
+  addAccessor(ctx: Context, mode: 'query', baseKey: null | string, lambda: AnyLambda): BinDbQuery;
+  addAccessor(ctx: Context, mode: 'write', baseKey: null | string, lambda: AnyLambda): BinDbWrite;
+  addAccessor(ctx: Context, mode: 'admin', baseKey: null | string, lambda: AnyLambda): BinDbAdmin;
+  addAccessor(ctx: Context, mode: 'query' | 'write' | 'admin', baseKey: null | string, lambda: LambdaBase<any, any, any, any, any, any>): any {
     
     this.accessors.push({ mode, baseKey, lambda });
     
-    if (mode === 'keep') {
-      
-      return {
-        hoist: '<repo>/src/node/lilac/comm/lambdaStorage/keep::LambdaStorageKeeper',
-        form: LambdaStorageKeeper,
-        args: [ this.getAccessorConfig(ctx, baseKey) ]
-      } as JsfnInst<typeof LambdaStorageKeeper>;
-      
-    } else if (mode === 'view') {
-      
-      return {
-        hoist: '<repo>/src/node/lilac/comm/lambdaStorage/view::LambdaStorageViewer',
-        form: LambdaStorageViewer,
-        args: [ this.getAccessorConfig(ctx, baseKey) ]
-      } as JsfnInst<typeof LambdaStorageViewer>;
-      
-    } else if (mode === 'mark') {
-      
-      return {
-        hoist: '<repo>/src/node/lilac/comm/lambdaStorage/mark::LambdaStorageMarker',
-        form: LambdaStorageMarker,
-        args: [ this.getAccessorConfig(ctx, baseKey) ]
-      } as JsfnInst<typeof LambdaStorageMarker>;
-      
-    }
+    if (mode === 'admin') return new BinDbAdmin(this.getAccessorConfig(ctx, baseKey));
+    if (mode === 'query') return new BinDbQuery(this.getAccessorConfig(ctx, baseKey));
+    if (mode === 'write') return new BinDbWrite(this.getAccessorConfig(ctx, baseKey));
     
-    throw Error('bad mode')[mod]({ mode });
+    throw Error('bad mode')[cl.mod]({ mode });
     
   }
   
-  async getTfEntities0(ctx: LilacContext) {
+  async getPetals(ctx: Context & { soil: Soil.Base }) {
     
-    const entities: TfEntity[] = [];
-    const addEntity = (ent: TfEntity) => { entities.push(ent); return ent; };
+    const entities: PetalTerraform.Base[] = [];
+    const addEntity = (petal: PetalTerraform.Base) => (entities.push(petal), petal);
     
     const bucket = addEntity(this.getBucket(ctx));
     
-    const ownership = addEntity(new TfResource('awsS3BucketOwnershipControls', this.name, {
-      bucket: bucket.tfRefp('bucket'),
+    const ownership = addEntity(new PetalTerraform.Resource('awsS3BucketOwnershipControls', this.name, {
+      bucket: bucket.ref('bucket'),
       $rule: {
-        objectOwnership: capitalize('objectWriter')
+        objectOwnership: phrasing('camel->kamel', 'objectWriter')
       }
     }));
     
-    const accessControlList = addEntity(new TfResource('awsS3BucketAcl', this.name, {
-      bucket: bucket.tfRefp('bucket'),
+    addEntity(new PetalTerraform.Resource('awsS3BucketAcl', this.name, {
+      bucket: bucket.ref('bucket'),
       acl: 'private',
-      dependsOn: [ ownership.tfRefp() ]
+      dependsOn: [ ownership.ref() ]
     }));
     
     for (const { mode, baseKey, lambda } of this.accessors) {
       
-      const roleTfEnt = await lambda.getRole().getTfEntities(ctx).then(ents => ents.find(ent => ent.getType() === 'awsIamRole')!);
+      const rolePetal = lambda.getRolePetal(ctx);
       
-      const lambdaPolicyName = capitalize([ 'lambdaStorage', lambda.getName(), this.name ]);
-      const lambdaPolicy = addEntity(new TfResource('awsIamPolicy', lambdaPolicyName, {
+      // const rolePetals = await superToArr(lambda.getRole().getPetals({ ...ctx, soil: null as any }));
+      // const rolePetal = rolePetals.find(ent => ent.getType() === 'awsIamRole')!;
+      
+      const lambdaPolicyName = `lambdaStorage${phrasing('camel->kamel', lambda.getName())}${phrasing('camel->kamel', this.name)}`;
+      const lambdaPolicy = addEntity(new PetalTerraform.Resource('awsIamPolicy', lambdaPolicyName, {
         name: `${ctx.pfx}-${lambdaPolicyName}`,
         policy: tf.json(aws.capitalKeys({ version: '2012-10-17', statement: [{
-          effect: capitalize('allow'),
+          effect: phrasing('camel->kamel', 'allow'),
           action: [
             
             's3:ListBucket', // Always include this??
-            ...([ 'view', 'keep' ][has](mode) ? [ 's3:GetObject', ] : []),
-            ...([ 'mark', 'keep' ][has](mode) ? [ 's3:PutObject', 's3:DeleteObject' ] : []),
+            ...([ 'query', 'admin' ][cl.has](mode) ? [ 's3:GetObject', ] : []),
+            ...([ 'write', 'admin' ][cl.has](mode) ? [ 's3:PutObject', 's3:DeleteObject' ] : []),
             // ...([ 'keep' ][has](mode)         ? [] : [])
             
           ],
           resource: [
-            `${tf.embed(bucket.tfRef('arn'))}`, // Consider: this is redundant unless using s3:ListObject, s3:GetBucketLocation, etc??
-            `${tf.embed(bucket.tfRef('arn'))}/${baseKey ?? '*'}`,
+            `${tf.embed(bucket.ref('arn'))}`, // Consider: this is redundant unless using s3:ListObject, s3:GetBucketLocation, etc??
+            `${tf.embed(bucket.ref('arn'))}/${baseKey ?? '*'}`,
           ]
         }]}))
       }));
       
-      const lambdaPolicyAttachment = addEntity(new TfResource('awsIamRolePolicyAttachment', lambdaPolicyName, {
-        role:      roleTfEnt.tfRefp('name'),
-        policyArn: lambdaPolicy.tfRefp('arn')
+      addEntity(new PetalTerraform.Resource('awsIamRolePolicyAttachment', lambdaPolicyName, {
+        role:      rolePetal.ref('name'),
+        policyArn: lambdaPolicy.ref('arn')
       }));
       
     }
